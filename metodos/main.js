7f86e85b828dce3f3e1e805f402a66e784fa7753
brendagq <brenda.gonzalezq@gmail.com>
2022-08-08 19:28:01 -0600

    Carpeta de metodos

---
+++ metodos/main.js
@@ -0,0 +1,61 @@
+
+const pets = [
+    {
+        specie: "Gato",
+        name: "Juno",
+        age: 5
+    },
+    {
+        specie: "Gato",
+        name: "Canela",
+        age: 1
+    },
+    {
+        specie: "Perro",
+        name: "Jupiter",
+        age: 6
+    },
+    {
+        specie: "Perro",
+        name: "Ramon",
+        age: 8
+    },
+    {
+        specie: "Ajolote",
+        name: "Nemo",
+        age: 1
+    },
+    {
+        specie: "Hamster",
+        name: "Benito",
+        age: 2
+    },
+    {
+        specie: "Gato",
+        name: "Aurora",
+        age: 3
+    }
+]
+
+const numbersArray = [ 10.5, 5.33, 3.4, 50.2, 33.888, 20, 101.83, 666.666 ]
+
+
+//map
+//crea un nuevo array con los valores de retorno del callback que recibe como parametro y que se aplica a cada uno de sus elementos
+
+//Generar un nuevo arreglo con los numeros redondeados.
+
+
+//filter
+//Filtrar a las mascotas por especie.
+
+//Generar una funcion que reciba como parametro un numero entero. Este numero se utilizara para filtrar a las mascotas. Aquellas que tengan una edad igual o mayor al parametro recibido se devolveran en un arreglo. En caso de que ninguna mascota entre en ese filtro, devolver una cadena que diga: "No se encontraron coincidencias"
+
+
+//find
+//evuelve el valor del primer elemento del array que cumple la función de prueba proporcionada.
+
+
+//Encontrar el primer numero que sea menor mayor a 15
+
+//Una funcion que devuelve a una mascota en especifico. La busqueda se llevara a cabo con el nombre.  
